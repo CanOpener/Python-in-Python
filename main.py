ee7f737e5e2513df313bdcf43e1b41244ba529d0
Mladen Kajic <mladen.kajic2@mail.dcu.ie>
2015-04-14 14:16:21 +0100

    snake dynamics upgrade
    removed bodyBlock.py as each block of the body can just be a two element
    list of x and y cos. not sure if i regret this decision yet.
    
    snake can change direction, eat apples and grow for now. still much to
    do

--- main.py
+++ main.py
@@ -1,18 +1,43 @@
 import pygame
 import colours
 import snake
-import bodyBlock
 pygame.init()
 
 gameDisplay = pygame.display.set_mode((800, 600))
+clock = pygame.time.Clock()
+snakey = snake.Snake([400, 500], 10, colours.blue, colours.black)
+apple = [100, 100]
 pygame.display.set_caption("Pyhton in Python")
 
 
+def clear() :
+    gameDisplay.fill(colours.white)
+
+def update() :
+    snakey.move(pygame, gameDisplay)
+    if snakey.head == apple :
+        snakey.needsGrowth = True
+    else :
+        pygame.draw.rect(gameDisplay, colours.red, [apple[0],apple[1],10,10])
+
+def render() :
+    pygame.display.update()
+
+def eventHandler() :
+    for event in pygame.event.get() :
+        if event.type == pygame.QUIT :
+            pygame.quit()
+            quit()
+        elif event.type == pygame.KEYDOWN :
+            snakey.dirChange(event, pygame)
+
 while True :
 
-    gameDisplay.fill(colours.white)
-    pygame.draw.rect(gameDisplay, colours.black, [20,20,10,10])
-    pygame.display.update()
+    clear()
+    eventHandler()
+    update()
+    render()
+    clock.tick(30)
 
 pygame.quit()
 quit()
--- snake.py
+++ snake.py
@@ -1,30 +1,61 @@
-import bodyBlock
-
-class snake :
+class Snake :
+    head = None
     body = []
-    bodyBlockSize = None
-    bodyBlockColour = None
-    headBlockColour = None
+    size = 0
+    bodyBlockSize = 0
+    direction = 1
+    headColour = None
+    bodyColour = None
+    needsGrowth = False
 
-    def move(self, posX, posY) :
+    def __init__(self, locationList, blockSize, hc, bc) :
+        self.headColour = hc
+        self.bodyColour = bc
+        self.bodyBlockSize = blockSize
 
-        priorX = posX
-        priorY = posY
+        self.head = locationList
 
-        for bodyPart in body :
+    def move(self, pygame, gameDisplay) :
+        previous = [self.head[0], self.head[1]] #force pass by value
 
-            tempX = bodyPart.xPos
-            tempY = bodyPart.yPos
-            bodyPart.xPos = priorX
-            bodyPart.yPos = priorY
-            priorX = tempX
-            priorY = tempY
-            bodyPart.drawSelf()
+        if self.direction == 1 : #up
+            self.head[1] -= self.bodyBlockSize
+        elif self.direction == 2 : #down
+            self.head[1] += self.bodyBlockSize
+        elif self.direction == 3 : #left
+            self.head[0] -= self.bodyBlockSize
+        elif self.direction == 4 : #right
+            self.head[0] += self.bodyBlockSize
 
-    def __init__(self, size, x, y, hc, bc) :
-        self.bodyBlockSize = size
-        self.headBlockColour = hc
-        self.bodyBlockColour = bc
+        pygame.draw.rect(gameDisplay, self.headColour, [self.head[0],self.head[1],self.bodyBlockSize,self.bodyBlockSize])
 
-        head = bodyBlock(x, y, hc, size)
-        body.push(head)
+        for bodyBlock in self.body :
+            temp =[bodyBlock[0], bodyBlock[1]]
+
+            bodyBlock[0] = previous[0]
+            bodyBlock[1] = previous[1]
+
+            previous = temp
+
+            pygame.draw.rect(gameDisplay, self.bodyColour, [bodyBlock[0],bodyBlock[1],self.bodyBlockSize,self.bodyBlockSize])
+
+        if self.needsGrowth :
+            self.body.append(previous)
+            pygame.draw.rect(gameDisplay, self.bodyColour, [previous[0],previous[1],self.bodyBlockSize,self.bodyBlockSize])
+            self.size+=1
+            self.needsGrowth = False
+
+
+    def dirChange(self, event, pygame) :
+        if event.key == pygame.K_UP :
+            if self.direction != 2 :
+                self.direction = 1
+        elif event.key == pygame.K_DOWN :
+            if self.direction != 1 :
+                self.direction = 2
+        elif event.key == pygame.K_LEFT :
+            if self.direction != 4 :
+                self.direction = 3
+        elif event.key == pygame.K_RIGHT :
+            if self.direction != 3 :
+                self.direction = 4
