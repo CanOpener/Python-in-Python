194d22dac0d3c8a317aa7b8f61a5e674ec73ebeb
Mladen Kajic <mladen.kajic2@mail.dcu.ie>
2015-04-14 17:59:42 +0100

    Revert "Revert "Collision, apples, difficulty and score""
    This reverts commit 056a6dcef30ada75f10f00e87732eae2f551169e.

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import pygame
+import random
 import colours
 import snake
+import block
 pygame.init()
 
-gameDisplay = pygame.display.set_mode((800, 600))
+FRAMEX = 800
+FRAMEY = 600
+BOUNDSX = 800
+BOUNDSY = 600
+FPS = 20
+pastSize = 0
+
+gameDisplay = pygame.display.set_mode((FRAMEX, FRAMEY))
 clock = pygame.time.Clock()
-snakey = snake.Snake([400, 500], 10, colours.blue, colours.black)
-apple = [100, 100]
+snakey = snake.Snake([400, 500], 10, colours.blue, colours.black, pygame, gameDisplay)
+apple = block.Block([BOUNDSX/2, BOUNDSY/2], colours.red, pygame, gameDisplay, 10)
 pygame.display.set_caption("Pyhton in Python")
 
 
@@ -14,23 +23,39 @@ def clear() :
     gameDisplay.fill(colours.white)
 
 def update() :
-    snakey.move(pygame, gameDisplay)
-    if snakey.head == apple :
+    global FPS
+    global pastSize
+    snakey.move()
+
+    if snakey.head.getCoordinates() == apple.getCoordinates() :
         snakey.needsGrowth = True
+        newX = random.randint(0, (FRAMEX-10)/10) * 10
+        newY = random.randint(0, (FRAMEY-10)/10) * 10
+        apple.x = newX
+        apple.y = newY
     else :
-        pygame.draw.rect(gameDisplay, colours.red, [apple[0],apple[1],10,10])
+        if not snakey.badMove(BOUNDSX, BOUNDSY) :
+            pygame.quit()
+            quit()
+
+    apple.draw()
+    snakey.turnedThisTurn = False
+    if (snakey.size != pastSize) :
+        pastSize = snakey.size
+        if (snakey.size%2 == 0) and (snakey.size != 0) :
+            FPS += 1
+    pygame.display.set_caption("Pyhton in Python - " + str(snakey.size))
 
 def render() :
     pygame.display.update()
 
 def eventHandler() :
-    snakey.turnedThisTurn = False
     for event in pygame.event.get() :
         if event.type == pygame.QUIT :
             pygame.quit()
             quit()
         elif event.type == pygame.KEYDOWN :
-            snakey.dirChange(event, pygame)
+            snakey.dirChange(event)
 
 while True :
 
@@ -38,7 +63,7 @@ while True :
     eventHandler()
     update()
     render()
-    clock.tick(15)
+    clock.tick(FPS)
 
 pygame.quit()
 quit()
--- snake.py
+++ snake.py
@@ -1,3 +1,5 @@
+import block
+
 class Snake :
     head = None
     body = []
@@ -8,59 +10,76 @@ class Snake :
     bodyColour = None
     needsGrowth = False
     turnedThisTurn = False
+    pygame = None
+    gameDisplay = None
 
-    def __init__(self, locationList, blockSize, hc, bc) :
+    def __init__(self, locationList, blockSize, hc, bc, pGame, gDisplay) :
         self.headColour = hc
         self.bodyColour = bc
         self.bodyBlockSize = blockSize
+        self.pygame = pGame
+        self.gameDisplay = gDisplay
 
-        self.head = locationList
+        self.head = block.Block(locationList, hc, pGame, gDisplay, blockSize)
 
-    def move(self, pygame, gameDisplay) :
-        previous = [self.head[0], self.head[1]] #force pass by value
+    def move(self) :
+        previous = self.head.getCoordinates()
 
         if self.direction == 1 : #up
-            self.head[1] -= self.bodyBlockSize
+            self.head.y -= self.bodyBlockSize
         elif self.direction == 2 : #down
-            self.head[1] += self.bodyBlockSize
+            self.head.y += self.bodyBlockSize
         elif self.direction == 3 : #left
-            self.head[0] -= self.bodyBlockSize
+            self.head.x -= self.bodyBlockSize
         elif self.direction == 4 : #right
-            self.head[0] += self.bodyBlockSize
+            self.head.x += self.bodyBlockSize
 
-        pygame.draw.rect(gameDisplay, self.headColour, [self.head[0],self.head[1],self.bodyBlockSize,self.bodyBlockSize])
+        self.head.draw()
 
         for bodyBlock in self.body :
-            temp =[bodyBlock[0], bodyBlock[1]]
+            temp = bodyBlock.getCoordinates()
 
-            bodyBlock[0] = previous[0]
-            bodyBlock[1] = previous[1]
+            bodyBlock.x = previous[0]
+            bodyBlock.y = previous[1]
 
             previous = temp
-
-            pygame.draw.rect(gameDisplay, self.bodyColour, [bodyBlock[0],bodyBlock[1],self.bodyBlockSize,self.bodyBlockSize])
+            bodyBlock.draw()
 
         if self.needsGrowth :
-            self.body.append(previous)
-            pygame.draw.rect(gameDisplay, self.bodyColour, [previous[0],previous[1],self.bodyBlockSize,self.bodyBlockSize])
+            newBlock = block.Block(previous, self.bodyColour, self.pygame, self.gameDisplay, self.bodyBlockSize)
+            self.body.append(newBlock)
+            newBlock.draw()
             self.size+=1
             self.needsGrowth = False
+            print(self.size)
 
 
-    def dirChange(self, event, pygame) :
+    def dirChange(self, event) :
         if self.turnedThisTurn :
             return
 
-        if event.key == pygame.K_UP :
+        if event.key == self.pygame.K_UP :
             if self.direction != 2 :
                 self.direction = 1
-        elif event.key == pygame.K_DOWN :
+        elif event.key == self.pygame.K_DOWN :
             if self.direction != 1 :
                 self.direction = 2
-        elif event.key == pygame.K_LEFT :
+        elif event.key == self.pygame.K_LEFT :
             if self.direction != 4 :
                 self.direction = 3
-        elif event.key == pygame.K_RIGHT :
+        elif event.key == self.pygame.K_RIGHT :
             if self.direction != 3 :
                 self.direction = 4
         self.turnedThisTurn = True
+
+    def badMove(self,boundx, boundy) :
+        headCO = self.head.getCoordinates()
+        if self.head.x > boundx or self.head.x < 0 :
+            return False
+        elif self.head.y > boundy or self.head.y <0 :
+            return False
+        else :
+            for bodyBlock in self.body :
+                if bodyBlock.getCoordinates() == headCO :
+                    return False
+        return True
